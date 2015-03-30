aa05c7fbbc734e58c40bf67c69c56ce64e9cb67e
dinar6 <didem.yanardag@bil.omu.edu.tr>
2015-03-30 12:46:42 +0300

    form oluşturuldu.

---
+++ form.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>form</title>
+</head>
+<body bgcolor="powderblue">
+<form action = "kontrol.php" method = "post" >
+<h1 align="center">Kayit Formu</h1>
+<center>
+<table cellpadding = "7" cellspacing = "7" bgcolor = #7F7F7F >
+<tr><td>Ad-Soyad:</td><td><input type = "text" name = "adSoyad"/><td></tr>
+<tr><td>Yaş:</td><td><input type = "text" name = "yas"/><td></tr>
+<tr><td>Şehir:</td><td><select name = "sehir"/>
+<option value = "eskisehir">Eskişehir</option>
+<option value = "istanbul">İstanbul</option>
+<option value = "ankara">Ankara</option></select><td></tr>
+<tr><td></td><td><input type = "submit" value = "Gönder"/></td></tr>
+</table></center>
+</form>
+</body></html>
